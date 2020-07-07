ecf9c0df0b0da9a2b97ce9fd0732d0391f53fd5a
behdad setoodegan <behdadsetoodegan@gmail.com>
2020-07-07 19:38:20 +1000

    home page edited

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-
+ 
 class Home extends Component {
     render() {
         return (
             <div>
                 <Header />
-                <Title />
-                <Login />
+                <h1>Icarus Airlines</h1>
+                <LoginForm />
             </div>
         )
     }
@@ -16,33 +16,40 @@ class Header extends Component {
     render() {
         return (
             <div>
-                
                 <a href="">Sign up</a>
             </div>
         )
     }
 }
 
-class Title extends Component {
-    render() {
+class LoginForm extends Component {
+    constructor(){
+        super();
+        this.state = {content: '', pass: ''}
+        this._handleChange=this._handleChange.bind(this);
+        this._handlePassChange=this._handlePassChange.bind(this);
+    }
+    
+    _handleChange(event){
+        this.setState({content: event.target.value})
+    }
+
+    _handlePassChange(event){
+        this.setState({pass: event.target.value})
+    }
+
+    render(){
         return (
-            <div>
-                <h1>Icarus Airlines</h1>
-            </div>
+            <form >
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" value={this.state.content} required="required" onChange={this._handleChange}></input>
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password" value={this.state.pass} required="required" onChange={this._handlePassChange}></input>
+                <input type="submit" name="commit" value="Login"></input>
+            </form>
         )
     }
-}
-
-function Login() {
-    return (
-        <form >
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email" value="" required="required"></input>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password" value="" required="required"></input>
-            <input type="submit" name="commit" value="Login"></input>
-        </form>
-    )
+    
 }
 
 
